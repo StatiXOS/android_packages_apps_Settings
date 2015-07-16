31410c0a4b9e314b237954113fe8d9805a1e373f
Chaitanya Saggurthi <csaggurt@codeaurora.org>
2015-07-16 16:33:34 +0530

    Add Manual provisioning support
    Add toggle switch UI option in SimSettings screen to
    enable/disable SIM cards
    
    Change-Id: Id07271aedb12eabc0b106ac278ba2e12c71cfe03

--- Android.mk
+++ Android.mk
@@ -44,6 +44,10 @@ LOCAL_JAVA_LIBRARIES := \
     telephony-common \
     ims-common
 
+ifeq ($(BOARD_VENDOR),oneplus)
+LOCAL_JAVA_LIBRARIES := telephony-ext
+endif
+
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-arch-lifecycle-runtime \
     android-arch-lifecycle-extensions \
