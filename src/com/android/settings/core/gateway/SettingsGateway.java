c2e0ff2c579d3c7ca1739155acba847e6b55b617
xyyx <xyyx@mail.ru>
2017-09-11 10:30:22 +0800

    Buttons: Add options for button brightness
    Change-Id: I9e554cf442a91b819589a151c8861ed6fa67990c
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -130,6 +130,7 @@ import com.android.settings.security.SecuritySettings;
 import com.android.settings.shortcut.CreateShortcut;
 import com.android.settings.sim.SimSettings;
 import com.android.settings.support.SupportDashboardActivity;
+import com.android.settings.system.ButtonSettings;
 import com.android.settings.system.ResetDashboardFragment;
 import com.android.settings.system.SystemDashboardFragment;
 import com.android.settings.tts.TextToSpeechSettings;
@@ -260,6 +261,7 @@ public class SettingsGateway {
             WifiAPITest.class.getName(),
             WifiInfo.class.getName(),
             MasterClear.class.getName(),
+            ButtonSettings.class.getName(),
             ResetDashboardFragment.class.getName(),
             NightDisplaySettings.class.getName(),
             ManageDomainUrls.class.getName(),
