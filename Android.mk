5ab0dacdf0202302c6b995aeb39870b29b991795
bigrushdog <randall.rushing@gmail.com>
2018-10-06 19:12:07 -0700

    Initial checkin for SmartNav navigation suite [2/4]
    Change-Id: I4b68d92e3bf3e6e5653d964e43e616ce4ca94d7a

--- Android.mk
+++ Android.mk
@@ -20,6 +20,13 @@ LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SRC_FILES += $(call all-java-files-under, ../Sparks/src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../SmartNavSettings/src)
+
+LOCAL_FULL_LIBS_MANIFEST_FILES += $(LOCAL_PATH)/AndroidManifest-SmartNav.xml
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DIR += packages/apps/Sparks/res
+LOCAL_RESOURCE_DIR += packages/apps/SmartNavSettings/res
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-slices-builders \
@@ -53,7 +60,8 @@ LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     frameworks/support/v7/appcompat/res \
     frameworks/support/v7/recyclerview/res \
     frameworks/support/design/res \
-    packages/apps/Sparks/res
+    packages/apps/Sparks/res \
+    packages/apps/SmartNavSettings/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
