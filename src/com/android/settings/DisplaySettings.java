d112fcdb07e824df0f4f6cf2a53a10b75874b095
Shahan-mik3 <mik3dev93@gmail.com>
2018-11-13 15:21:42 +0530

    Settings: Move Notch settings to display
    *Move display cutout settings from developer settings to Display
    *Show only for devices with notch
    
    Change-Id: Ia31a1647da54fb0ec2b361238da888bb35a2b94f

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -23,11 +23,11 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.provider.SearchIndexableResource;
-
 import com.android.internal.hardware.AmbientDisplayConfiguration;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.AmbientDisplayCustomPreferenceController;
+import com.android.settings.development.EmulateDisplayCutoutPreferenceController;
 import com.android.settings.display.AccentPickerPreferenceController;
 import com.android.settings.display.AmbientDisplayPreferenceController;
 import com.android.settings.display.BrightnessLevelPreferenceController;
@@ -151,6 +151,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
         controllers.add(new ColorModePreferenceController(context));
         controllers.add(new DarkUIPreferenceController(context));
+        controllers.add(new EmulateDisplayCutoutPreferenceController(context));
         return controllers;
     }
 
