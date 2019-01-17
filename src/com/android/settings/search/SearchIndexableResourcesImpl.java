58626400ef99e12dfc2d1a520fa492ff3ed01781
Brett Montague <bmontagu@gmail.com>
2019-01-17 11:01:06 -0600

    Add custom features to search index [2/2]
    Change-Id: Icef6e6b2969ac610a29b6d38449fc5d42dd71b3a

--- src/com/android/settings/search/SearchIndexableResourcesImpl.java
+++ src/com/android/settings/search/SearchIndexableResourcesImpl.java
@@ -98,6 +98,19 @@ import com.android.settings.wfd.WifiDisplaySettings;
 import com.android.settings.wifi.ConfigureWifiSettings;
 import com.android.settings.wifi.WifiSettings;
 
+// Sparks
+import com.statix.sparks.fragments.buttons.ButtonBrightness;
+import com.statix.sparks.fragments.buttons.Buttons;
+import com.statix.sparks.fragments.extras.notification.BatteryLightSettings;
+import com.statix.sparks.fragments.statusbar.ClockOptions;
+import com.statix.sparks.fragments.gestures.Gestures;
+import com.statix.sparks.fragments.misc.Misc;
+import com.statix.sparks.fragments.statusbar.Statusbar;
+import com.statix.sparks.fragments.statusbar.TrafficIndicators;
+import com.statix.sparks.fragments.system.GlobalActionsSettings;
+import com.statix.sparks.fragments.system.SmartPixels;
+import com.statix.sparks.fragments.system.System;
+
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -191,6 +204,19 @@ public class SearchIndexableResourcesImpl implements SearchIndexableResources {
         addIndex(RecentLocationRequestSeeAllFragment.class);
         addIndex(PreviouslyConnectedDeviceDashboardFragment.class);
         addIndex(BluetoothDashboardFragment.class);
+
+        // Sparks
+        addIndex(ButtonBrightness.class);
+        addIndex(Buttons.class);
+        addIndex(BatteryLightSettings.class);
+        addIndex(ClockOptions.class);
+        addIndex(Gestures.class);
+        addIndex(Misc.class);
+        addIndex(Statusbar.class);
+        addIndex(TrafficIndicators.class);
+        addIndex(GlobalActionsSettings.class);
+        addIndex(SmartPixels.class);
+        addIndex(System.class);
     }
 
     @Override
