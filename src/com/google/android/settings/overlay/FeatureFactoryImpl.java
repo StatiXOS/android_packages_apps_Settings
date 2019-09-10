1fb56274c16b7320e2733721c6392d2e64e59c0c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-10 14:35:59 -0300

    FeatureFactoryImpl: Import PowerUsageFeatureProviderGoogleImpl
    Required since we ship Turbo app
    
    Change-Id: I4a69bad4a329e9913fdecc1ffc191f0ce1d7fb0e

--- src/com/google/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/google/android/settings/overlay/FeatureFactoryImpl.java
@@ -6,11 +6,27 @@ import com.google.android.settings.accounts.AccountFeatureProviderGoogleImpl;
 public final class FeatureFactoryImpl extends com.android.settings.overlay.FeatureFactoryImpl {
     private AccountFeatureProvider mAccountFeatureProvider;
 
+import com.android.settings.fuelgauge.PowerUsageFeatureProvider;
+import com.google.android.settings.accounts.AccountFeatureProviderGoogleImpl;
+import com.google.android.settings.fuelgauge.PowerUsageFeatureProviderGoogleImpl;
+
+public final class FeatureFactoryImpl extends com.android.settings.overlay.FeatureFactoryImpl {
+    private AccountFeatureProvider mAccountFeatureProvider;
+    private PowerUsageFeatureProvider mPowerUsageProvider;
+
     @Override
     public AccountFeatureProvider getAccountFeatureProvider() {
         if (mAccountFeatureProvider == null) {
             mAccountFeatureProvider = new AccountFeatureProviderGoogleImpl();
         }
         return mAccountFeatureProvider;
+    }
+
+    @Override
+    public PowerUsageFeatureProvider getPowerUsageFeatureProvider(Context context) {
+        if (mPowerUsageProvider == null) {
+            mPowerUsageProvider = new PowerUsageFeatureProviderGoogleImpl(context.getApplicationContext());
+        }
+        return mPowerUsageProvider;
     }
 }
