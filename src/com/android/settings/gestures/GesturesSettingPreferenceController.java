6a719174cff8de84c09627e415e17baf1a02eaa4
zzkW35 <zzkw35@gmail.com>
2019-11-23 09:15:40 -0800

    Double tap to sleep on lockscreen [2/2]
    Change-Id: I1eb13b8fac138eb45147c2b7e4447e30e5dff4dc

--- src/com/android/settings/gestures/GesturesSettingPreferenceController.java
+++ src/com/android/settings/gestures/GesturesSettingPreferenceController.java
@@ -78,6 +78,8 @@ public class GesturesSettingPreferenceController extends BasePreferenceControlle
                 .setConfig(ambientDisplayConfiguration));
         controllers.add(new PreventRingingParentPreferenceController(context, FAKE_PREF_KEY));
         controllers.add(new VolumeButtonMusicControlPreferenceController(context, FAKE_PREF_KEY));
+        controllers.add(new DoubleTapSleepLockscreenPreferenceController(context, FAKE_PREF_KEY));
+
         return controllers;
     }
 
