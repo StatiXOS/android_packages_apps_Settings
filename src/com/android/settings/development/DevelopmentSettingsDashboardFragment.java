92a1a2a84d826acb7c1fdcfa9253a2a0c69bcadd
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2022-01-30 03:43:39 -0500

    Settings: Move over refresh rate controls to Display
    * Move these preference to a completely separate category.
    
    * Don't block them if the user does not have developer options
      enabled.
    
    Change-Id: Ic67b32b949a3c5b4ff280449580016bf4d9b9d6b

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -647,7 +647,6 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         controllers.add(new WaitForDebuggerPreferenceController(context));
         controllers.add(new EnableGpuDebugLayersPreferenceController(context));
         controllers.add(new GraphicsDriverEnableAngleAsSystemDriverController(context, fragment));
-        controllers.add(new ForcePeakRefreshRatePreferenceController(context));
         controllers.add(new EnableVerboseVendorLoggingPreferenceController(context));
         controllers.add(new VerifyAppsOverUsbPreferenceController(context));
         controllers.add(new ArtVerifierPreferenceController(context));
@@ -677,7 +676,6 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         controllers.add(new PointerLocationPreferenceController(context));
         controllers.add(new ShowSurfaceUpdatesPreferenceController(context));
         controllers.add(new ShowLayoutBoundsPreferenceController(context));
-        controllers.add(new ShowRefreshRatePreferenceController(context));
         controllers.add(new RtlLayoutPreferenceController(context));
         controllers.add(new WindowAnimationScalePreferenceController(context));
         controllers.add(new EmulateDisplayCutoutPreferenceController(context));
