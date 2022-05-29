9ee1cd3264f1b74362a098dde03acbea85542b1e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-05-30 02:15:47 +0530

    Settings: Fix loading iconpack-based Wellbeing icon
    Wellbeing apk still provides Android 11 style settings icon
    when an icon pack is set. It consists of a layer-list with
    circle shape and drawable, so fetch the drawable to make
    it work with Android 12 settings.
    
    [sourajitk: make sure iconPackage is not null]
    
    Change-Id: I78be2db9dc8ca262e61fdb13b93da007c3a6eb81

--- src/com/android/settings/dashboard/DashboardFeatureProviderImpl.java
+++ src/com/android/settings/dashboard/DashboardFeatureProviderImpl.java
@@ -101,6 +101,7 @@ public class DashboardFeatureProviderImpl implements DashboardFeatureProvider {
     private static final String TAG = "DashboardFeatureImpl";
     private static final String DASHBOARD_TILE_PREF_KEY_PREFIX = "dashboard_tile_pref_";
     private static final String META_DATA_KEY_INTENT_ACTION = "com.android.settings.intent.action";
+    private static final String WELLBEING_PACKAGE = "com.google.android.apps.wellbeing";
     private static final String TOP_LEVEL_ACCOUNT_CATEGORY = "top_level_account_category";
 
     @VisibleForTesting
@@ -542,6 +543,17 @@ public class DashboardFeatureProviderImpl implements DashboardFeatureProvider {
                     return;
                 }
                 iconDrawable.setTint(Utils.getHomepageIconColor(preference.getContext()));
+            }
+            if (WELLBEING_PACKAGE.equals(iconPackage) && iconDrawable instanceof LayerDrawable
+                 && ((LayerDrawable) iconDrawable).getDrawable(1) != null) {
+                iconDrawable = ((LayerDrawable) iconDrawable).getDrawable(1);
+                iconDrawable.mutate();
+            }
+            // Skip tinting and Adaptive Icon transformation for homepage account type raw icons
+            if (TextUtils.equals(tile.getGroupKey(), "top_level_account_category")
+                    && iconPackage == null) {
+                preference.setIcon(iconDrawable);
+                return;
             }
 
             if (forceRoundedIcon && !TextUtils.equals(mContext.getPackageName(), iconPackage)) {
