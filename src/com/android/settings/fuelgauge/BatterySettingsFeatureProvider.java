df494b0957fe30d484a1bd8dabbf810659418598
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2024-06-13 11:58:21 -0400

    Revert "Config battery info page(1/2)"
    This reverts commit bace563189d1bf3a6fdee2381c89a354fcc442ec.

--- src/com/android/settings/fuelgauge/BatterySettingsFeatureProvider.java
+++ src/com/android/settings/fuelgauge/BatterySettingsFeatureProvider.java
@@ -35,9 +35,6 @@ public interface BatterySettingsFeatureProvider {
     /** Returns true if first use date should be shown */
     boolean isFirstUseDateAvailable(Context context, long firstUseDateMs);
 
-    /** Check whether the battery information page is enabled in the About phone page */
-    boolean isBatteryInfoEnabled(Context context);
-
     /** A way to add more battery tip detectors. */
     void addBatteryTipDetector(
             Context context,
